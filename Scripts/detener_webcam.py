a2c5632ba778f9f2df6f8dc37c9ef71cfbdefe49
Alecaialex <46007072+Alecaialex@users.noreply.github.com>
2023-05-31 10:50:05 +0200

    31/5/1

--- Scripts/detener_webcam.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-#Introduce la ID y el puerto elegidos anteriormente
-url = 'http://192.168.0.28:8000'
-
-#Enviar solicitud POST con el texto "cerrar"
-response = requests.post(url, data='cerrar')
-print("Solicitud enviada")
